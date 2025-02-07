07cb1f6b3d150cb1942a46d32182de37b0ac6bcb
Ravinamohite07 <ravinamohite527@gmail.com>
2025-02-07 13:14:30 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use("/",(req,res)=>{
+    res.send("hello from /");
+})
+
+app.use("/test",(req,res)=>{
+    res.send("test...!");
+})
+
+
+app.use("/hello",(req,res)=>{
+    res.send("hello...!");
+})
+
+
+
+app.listen(7777,() =>{
+console.log("server listen on port 7777...")
+})
---
+++ src/app3.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("test...!");
+})
+
+app.use("/hello/2",(req,res)=>{
+    res.send("hello...!");
+})
+
+app.use("/",(req,res)=>{
+   res.send("hello from /");
+})
+
+app.listen(7777,() =>{
+    console.log("server listen on port 7777...")
+    })
